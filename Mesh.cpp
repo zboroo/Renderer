91f9161e417215fdad3e595f001f995f3fa4bf05
zboroo <zboroo@163.com>
2019-05-27 19:52:51 +0800

    补充Mesh

--- Mesh.cpp
+++ Mesh.cpp
@@ -19,11 +19,29 @@ Mesh::Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, std::vecto
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GL_UNSIGNED_INT) * indices.size(), indices.data(), GL_STATIC_DRAW);
 
 	GLint positionLocation = glGetAttribLocation(shader.program, "position");
+	glEnableVertexAttribArray(positionLocation);
+	glVertexAttribPointer(positionLocation, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
 
+	GLint normalLocation = glGetAttribLocation(shader.program, "normal");
+	glEnableVertexAttribArray(normalLocation);
+	glVertexAttribPointer(normalLocation, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
 
+	GLint texcoordLocation = glGetAttribLocation(shader.program, "texcoord");
+	glEnableVertexAttribArray(texcoordLocation);
+	glVertexAttribPointer(texcoordLocation, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texcoord));
+
+	GLint tangentLocation = glGetAttribLocation(shader.program, "tangent");
+	glEnableVertexAttribArray(tangentLocation);
+	glVertexAttribPointer(tangentLocation, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tangent));
+
+	GLint bitangentLocation = glGetAttribLocation(shader.program, "bitangent");
+	glEnableVertexAttribArray(bitangentLocation);
+	glVertexAttribPointer(bitangentLocation, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, bitangent));
 }
 
 Mesh::~Mesh()
 {
-
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &IBO);
 }
